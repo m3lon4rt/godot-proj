e4030a0ce0dc165437934cdfcb82579299ade9ec
Nakaharu Amahara <amaharanakaharu@gmail.com>
2023-09-23 12:01:08 -0400

    Finished basic 'Walker' enemy that follows the player

--- Scenes/Walker_mob.gd
+++ Scenes/Walker_mob.gd
@@ -2,7 +2,6 @@ class_name Walker
 extends CharacterBody2D
 
 
-const SPEED = 300.0
 @export var MAX_SPEED = 40.0
 @export var ACCELERATION = 50.0
 @onready var ray_cast_2d = $RayCast2D
@@ -22,15 +21,14 @@ func _ready():
 	magic_hp_count = magic_hp
 
 func _physics_process(delta):
-	# Debug for HP values
-	print("physic_hp: %d" % physic_hp_count)
+	# Debug for HP values)
 	get_node("PhysicHP").text = str(physic_hp)
 	
-	print("magic_hp: %d" % magic_hp_count)
 	get_node("MagicHP").text = str(magic_hp)
 	
-	print("target posx: %d" % target.global_position.x)
-	print("target posy: %d" % target.global_position.y)
+	print("raycast direction: %d" % ray_cast_2d.target_position.x)
+	
+	print("velocity.x: %d" % velocity.x)
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -38,5 +36,18 @@ func _physics_process(delta):
 	
 	ray_cast_2d.target_position = target.global_position - ray_cast_2d.global_position + Vector2(16,16)
 	# added the last bit to compensate for the player's origin being at its upper left
+	
+	var player_direction = ray_cast_2d.target_position.x
+	if player_direction > 0:
+		velocity.x += ACCELERATION
+	elif player_direction < 0:
+		velocity.x -= ACCELERATION
+	else:
+		velocity.x = 0
+		
+	if velocity.x < -MAX_SPEED:
+		velocity.x = -MAX_SPEED
+	elif velocity.x > MAX_SPEED:
+		velocity.x = MAX_SPEED
 	
 	move_and_slide()
