162a0c707abdb2f853f1e136786e774753e78cc3
m3lon4rt <m3lon0337@gmail.com>
2023-09-25 10:45:29 +0800

    Clean up code

--- Scenes/Basic_Atk.gd
+++ Scenes/Basic_Atk.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-var atk_decay = 5
+# I had to add a minute delay so the system registers any collisions
+var atk_decay = 0
+# Number of enteties hit in one attack
 var victims = 0
+# Total Hit counter (Could be useful for combos and whatnot, but for now it's just here)
 var hits = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -10,18 +13,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Point towards mouse
 	look_at(get_global_mouse_position())
-	atk_decay -= 1
+	
+	
 	
 	if Input.is_action_just_pressed("basic_attack"):
+		# Delay duration
 		atk_decay = 3
+		# Change color
 		self.modulate = Color(1, 1, 1)
+		# Turn on the hitbox
 		get_node("Basic_Atk_Hitbox").disabled = false
 	else:
+		if atk_decay > 0:
+			# Decrements decay by 1 overtime
+			atk_decay -= 1
 		if atk_decay == 0:
+			# Reset victim counter
 			victims = 0
+			# Change color
 			self.modulate = Color().hex(002211)
+			# Turn off the hitbox
 			get_node("Basic_Atk_Hitbox").disabled = true
 	
+	# Function that checks if any Entities are within the hitbox during the attack
 	if self.has_overlapping_bodies() and get_node("Basic_Atk_Hitbox").disabled == false:
 		hits += 1 * self.get_overlapping_bodies().size()
